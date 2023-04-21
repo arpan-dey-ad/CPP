7ecfacbf757f397535c28b36f5426f09f44bb03e
Arpan <arpan.dey.code@gmail.com>
2023-04-21 18:54:12 +0530

    init

--- test.cpp
+++ test.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-  cout << "Hello Everyone";
-  return 0;
+int main()
+{
+    cout<<"hello";
 }
--- test2.cpp
+++ test2.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
+using namespace std;
 int main()
 {
-    std::cout<<"Hello World";
-    return 0; 
+    int a, b, c;
+    cout<<"Enter thenumber: ";
+    cin>>a>>b;
+    cout<<"a / b is"<< (float)a / b;
 }
