b82ec3e12cbd62c51b34d135f835c67ba6f12439
Arpan <arpan.dey.code@gmail.com>
2023-04-24 11:42:13 +0530

    added line.cpp

--- line.cpp
+++ line.cpp
@@ -2,7 +2,7 @@
 #include<math.h>
 using namespace std;
 class line{
-    public:
+    private:
     int p1, p2;
     public:
     line(int p3, int p4)
@@ -10,17 +10,26 @@ class line{
         p1 = p3;
         p2 = p4;
     }
+    int getnum1()
+    {
+        return p1;
+    }
+    int getnum2()
+    {
+        return p2;
+    }
 };
 int main()
 {
     int x,y,z,w;
     cout<<"Enter the points";
     cin>>x>>y>>z>>w;
-    line a(x,y), b(z,w);
+    line a(x,y);
+    line b(z,w);
     int c,d;
     float e;
-    c = pow((b.p1)-(a.p1),2);
-    d = pow((b.p2)-(a.p2),2);
+    c = pow(((b.getnum1())-(a.getnum1())),2);
+    d = pow(((b.getnum2())-(a.getnum2())),2);
     e = sqrt(c+d);
     cout<<e;
     return 1;    
