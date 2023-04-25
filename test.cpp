fcb5d0cb6dd15466af1f0b93e4f353b259f83d15
Arpan <arpan.dey.code@gmail.com>
2023-04-25 21:06:30 +0530

    init

--- test.cpp
+++ test.cpp
@@ -1,6 +1,20 @@
-#include<iostream>
-using namespace std;
+include <algorithm>
+#include <iostream>
+  
 int main()
 {
-    cout<<"hello";
+    int arr[] = {3, 5, 1, 2, 4};
+  
+    // Sort the array in ascending order
+    std::sort(std::begin(arr), std::end(arr));
+  
+    // Print the sorted array
+    for (int i : arr)
+    {
+        std::cout << i << " ";
+    }
+  
+     
+  
+    return 0;
 }
